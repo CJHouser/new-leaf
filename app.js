9f30d19ceec78ed5e83ded41348e6d70afe9507b
CJHouser <cjohouser@gmail.com>
2021-04-06 16:06:04 -0700

    mass deletion

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // A bot that deletes old, profane messages from text channels
 
+const ph = require("perf_hooks");
 const routines = require("./routines.js");
 require("dotenv").config();
 const Discord = require("discord.js");
@@ -11,12 +12,28 @@ client.on("ready", () => {
 });
 
 client.on("message", async message => {
-  if (!routines.authenticate(message.member.permissionsIn(message.channel))) {
-    return;
-  }
+  if (!routines.authenticate(message.member.permissionsIn(message.channel))) return;
   if (!routines.validate(message)) return;
+  const start = ph.performance.now();
+  console.log("Started");
+  let messagesToDelete = new Discord.Collection();
   let lastInBatch = message;
-  while (lastInBatch) lastInBatch = await routines.clean(lastInBatch);
+  const options = {limit: 100, before: message.id};
+  while (lastInBatch) {
+    const messagesToCheck = await message.channel.messages.fetch(options);
+    tuple = await routines.clean(lastInBatch);
+    lastInBatch = tuple.lastInBatch;
+    messagesToDelete = messagesToDelete.concat(tuple.profaneMessages);
+  }
+  // Bulk deletion only works for messages younger than 14 days
+  const deletedMessages = await message.channel.bulkDelete(messagesToDelete, true);
+  // Difference returns the messages older than 14 days for one-by-one deletion
+  const olderMessagesToDelete = deletedMessages.difference(messagesToDelete);
+  console.log(deletedMessages.size);
+  console.log(olderMessagesToDelete.size)
+  olderMessagesToDelete.forEach(m => m.delete());
+  console.log(`Deleted ${messagesToDelete.size} messages`);
+  console.log(`Finished after ${(ph.performance.now() - start)/1000} seconds`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
