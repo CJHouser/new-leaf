554496e9bdb71c328ff600918807ba12b69561c1
CJHouser <cjohouser@gmail.com>
2021-04-26 13:36:38 -0700

    clean test scaffolding

--- test/test.js
+++ test/test.js
@@ -2,17 +2,31 @@ const MessageMock = require("./mocks/MessageMock.js");
 const ChannelMock = require("./mocks/ChannelMock.js");
 const Cleaner = require("../src/classes/Cleaner.js");
 
-var assert = require("assert");
+let assert = require("assert");
 
-const channel = new ChannelMock();
-for (let i = 0; i < 100; i++) {
-  if (i % 10 == 0) {
-    var message = new MessageMock("fuck " + i, channel);
-  }
-  else {
-    var message = new MessageMock("test " + i, channel);
-  }
+// Retrieves messages marked as deleted
+function getDeleted(chan) {
+  return chan.messages.coll.filter(m => m.deleted);
+}
+
+// Populate a mock channel with messages containing a set number of profanity
+const historySize = 111;
+const profaneDistribution = 10;
+const numberOfProfaneMessages = Math.ceil(historySize / profaneDistribution);
+let channel = new ChannelMock();
+{
+  let message = new MessageMock("!clean", channel);
   channel.messages.coll.set(message.id, message);
 }
-const cleaner = new Cleaner(message);
-cleaner.start();
+for (let i = 0; i < historySize; i++) {
+  let content = (i % profaneDistribution == 0) ? "fuck " + i : "test " + i;
+  let message = new MessageMock(content, channel);
+  channel.messages.coll.set(message.id, message);
+}
+const cleaner = new Cleaner(channel.messages.coll.first());
+
+// Reset the static IDs tied to each class before each test
+beforeEach(() => {
+  channel.messages.lastIndex = 0;
+  channel.messages.coll.map(m => m.deleted = false);
+});
