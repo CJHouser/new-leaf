be05e7735db74f105dd20f05343653181ca7a208
CJHouser <cjohouser@gmail.com>
2021-04-26 13:27:27 -0700

    recursive clean replaced with iterative implementation

--- src/commands/clean.js
+++ src/commands/clean.js
@@ -3,9 +3,11 @@ const Cleaner = require("../classes/Cleaner");
 module.exports = {
   name: "!clean",
   description: "Removes messages containing profanity from a Discord text channel.",
-  execute(message) {
-    message.reply("Cleaning started");
+  async execute(message) {
+    message.reply("Cleaning started.");
     const cleaner = new Cleaner(message);
-    cleaner.start();
+    await cleaner.start();
+    message.reply("Cleaning finished.");
+  },
   }
 };
