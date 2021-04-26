684ca4f34897d41ba347b6d1b0db8b4019e7084d
CJHouser <cjohouser@gmail.com>
2021-04-26 13:35:21 -0700

    simplified fetch() implementation

--- test/mocks/MessageManagerMock.js
+++ test/mocks/MessageManagerMock.js
@@ -1,21 +1,14 @@
-const { Collection, RichPresenceAssets } = require("discord.js");
+const { Collection } = require("discord.js");
 
 class MessageManagerMock {
   constructor() {
-    this.last = 0;
+    this.lastIndex = 0;
     this.coll = new Collection();
   }
 
   async fetch(options) {
-    let array = [];
-    let iter = this.coll.entries();
-    let result = iter.next();
-    while (!result.done && options.limit-- > 0) {
-      array.push(result.value);
-      result = iter.next();
-    }
-    if (this.last >= this.coll.size) this.last = 0
-    return new Collection(array);
+    this.lastIndex += options.limit;
+    return new Collection(Array.from(this.coll).slice(this.lastIndex - options.limit, this.lastIndex));
   }
 }
 
